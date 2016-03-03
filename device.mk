aab39abe2a342dc4fedaa5bef76a5055a1c4da7b
smoki3 <kevinbayer8@gmail.com>
2016-03-03 18:42:40 +0100

    Bullhead: Enable more Snap features
    Change-Id: I2e5e2961de845ab6cb2352dcd6b2fbc223aac83f

--- device.mk
+++ device.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
     mm-qcamera-app \
     Snap
 
+# Snap Config
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false
+
 # Sensor & activity_recognition HAL
 PRODUCT_PACKAGES += \
     sensors.bullhead \
