fd532293cd89e822cf24c109d302ca748b80e305
Dimitris Mantzouranis <d3xter93@gmail.com>
2018-02-18 17:58:30 +0100

    bullhead: update build fingerprint to OPM3.171019.014

--- slim.mk
+++ slim.mk
@@ -42,5 +42,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/NPF26F/3468883:user/release-keys \
-PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NPF26F 3468883 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM3.171019.014/4503998:user/release-keys \
+PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.014 4503998 release-keys"
